6fd6ceec2c81249d964b63fbc850c625885c3d5e
Lailansyahda <syahdaazalia123@gmail.com>
2025-06-02 18:45:21 +0700

    int main

--- noPewarisan/noPewarisan.cpp
+++ noPewarisan/noPewarisan.cpp
@@ -18,3 +18,11 @@ class derivedClass : public baseClass
         cout << "Hallo saya Function dari derived Class";
     }
 };
+
+int main()
+{
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+}
